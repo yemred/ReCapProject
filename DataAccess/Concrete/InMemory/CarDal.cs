e614ebb788072f8a5422f71b06ba40ccff7c3b93
emre <yemreduymaz@hotmail.com>
2021-03-15 20:32:13 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/CarDal.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class CarDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Car entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GeTById(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car entitiy)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
